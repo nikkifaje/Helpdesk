357a409d74431f77b05c864505fac8a65b312b6c
Nikki Fajemisin <nikkifaje@gmail.com>
2016-04-21 12:26:00 -0700

    Removed Extention

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,10 +30,7 @@ sys.path.append('VN/requeststest/Scripts/Doctut/docs/source')
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.githubpages',
-]
+extensions = ['sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['ntemplates']
